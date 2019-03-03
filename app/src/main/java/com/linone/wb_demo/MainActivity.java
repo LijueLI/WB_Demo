359273ad98265a4f6a7f40ba9e356f4618d2d9a8
黎晝 <boting87@gmail.com>
2019-03-03 23:37:22 +0800

    Add MPA

--- app/src/main/java/com/linone/wb_demo/MainActivity.java
+++ app/src/main/java/com/linone/wb_demo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.linone.wb_demo;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -20,51 +23,60 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int result = 1;
+    private int result = 30;
     private jsonObject[] Data = null;
-    private TextView Watertemp,Temp,Humi;
-    private Button get;
+    private TextView Watertemp,Temp,Humi,pH;
+    private LineChart watertempchart;
     private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Watertemp = findViewById(R.id.textView);
-        Temp = findViewById(R.id.textView2);
-        Humi = findViewById(R.id.textView3);
-        get = findViewById(R.id.get);
-        get.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getdata();
-            }
-        });
+        Watertemp = findViewById(R.id.WT_Data);
+        pH = findViewById(R.id.WP_Data);
+        Temp = findViewById(R.id.T_Data);
+        Humi = findViewById(R.id.H_Data);
+        watertempchart = findViewById(R.id.WaterTempChart);
 
+        XAxis WatertempxAxis = watertempchart.getXAxis();
+        WatertempxAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        WatertempxAxis.setEnabled(true);
+        WatertempxAxis.setDrawAxisLine(true);
+        WatertempxAxis.setDrawGridLines(false);
+        getdata();
     }
 
     private void getdata(){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try{
-                    String url = "https://api.thingspeak.com/channels/700874/feeds.html?results="+String.valueOf(result);
-                    Document document = Jsoup.connect(url).get();
-                    Elements elements = document.select("body");
-                    String S = elements.text();
-                    JsonObject j = gson.fromJson(S,JsonObject.class);
-                    JsonArray K = j.get("feeds").getAsJsonArray();
-                    Data = gson.fromJson(K,jsonObject[].class);
-                }catch (IOException e){
-                    Log.e("getdata()","connecterror");
-                }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Watertemp.setText("Watertemp : "+Data[0].getWatertemp());
-                        Temp.setText("Temp : "+Data[0].getTemp());
-                        Humi.setText("Humi : "+Data[0].getHumi());
+                while(true) {
+                    try {
+                        String url = "https://api.thingspeak.com/channels/700874/feeds.html?results=" + String.valueOf(result);
+                        Document document = Jsoup.connect(url).get();
+                        Elements elements = document.select("body");
+                        String S = elements.text();
+                        JsonObject j = gson.fromJson(S, JsonObject.class);
+                        JsonArray K = j.get("feeds").getAsJsonArray();
+                        Data = gson.fromJson(K, jsonObject[].class);
+                    } catch (IOException e) {
+                        Log.e("getdata()", "connecterror");
                     }
-                });
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Watertemp.setText(Data[29].getWatertemp() + "  °C");
+                            pH.setText(Data[29].getPH() + "      ");
+                            Temp.setText(Data[29].getTemp() + "  °C");
+                            Humi.setText(Data[29].getHumi() + "   %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(15000);
+                    }catch (InterruptedException e){
+                        Log.e("Sleep","sleeperror");
+                    }
+                }
             }
         }).start();
     }
